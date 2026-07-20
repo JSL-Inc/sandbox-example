53e3c55309945e38aa43920c9c3a038f71f96683
jacoblaw1 <saints8lol@gmail.com>
2026-07-20 11:21:46 -0500

    Add unit-test.sh for running tests and coverage

---
+++ .github/golden-path/unit-test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+mkdir -p reports/junit
+mkdir -p reports/coverage
+
+pytest \
+  --junitxml=reports/junit/results.xml \
+  --cov=. \
+  --cov-report=xml:reports/coverage/cobertura.xml
